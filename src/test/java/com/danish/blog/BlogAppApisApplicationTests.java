16f3cba0550bc3942613f9299923c08db450c6a5
studywithdanish <studywithdanish123@gmail.com>
2026-06-06 11:38:54 +0530

    Add isolated test database profile

--- src/test/java/com/danish/blog/BlogAppApisApplicationTests.java
+++ src/test/java/com/danish/blog/BlogAppApisApplicationTests.java
@@ -4,8 +4,10 @@ import com.danish.blog.repositories.UserRepo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BlogAppApisApplicationTests {
 
     @Autowired
